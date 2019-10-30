433602de0c892fd6f59371ab6aace8f5477e11ae
Chih-Ting Su <>
2019-10-30 15:27:34 +1100

    add style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Header, Body, Left, Right } from './style';
+import Codemirror from 'react-codemirror';
+import 'codemirror/lib/codemirror.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { config: 'abc' };
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Header>Header</Header>
+        <Body>
+          <Left>
+            <Codemirror value={this.state.config} />
+          </Left>
+          <Right>Right</Right>
+        </Body>
+      </Layout>
+    );
+  }
 }
+
+
+
 
 export default App;
